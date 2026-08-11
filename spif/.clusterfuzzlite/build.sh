e203162a83d0dfc01794c6ccbd9f3364ad7afd1d
jimkali <kalinovjim@gmail.com>
2026-08-11 07:56:10 -0400

    fix: quote paths in ClusterFuzzLite build.sh to survive whitespace/glob chars
    Addresses CodeRabbit nitpick on PR #49.

--- spif/.clusterfuzzlite/build.sh
+++ spif/.clusterfuzzlite/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eu
 pip3 install .
 
-for fuzzer in $(find $SRC -name '*_fuzzer.py'); do
-  compile_python_fuzzer $fuzzer
-done
+while IFS= read -r -d '' fuzzer; do
+  compile_python_fuzzer "$fuzzer"
+done < <(find "$SRC" -type f -name '*_fuzzer.py' -print0)
 
-zip -j $OUT/fuzz_decode_fuzzer_seed_corpus.zip $SRC/spif/spif/fixtures/cross_lang/*.spif
-zip -j $OUT/fuzz_decode_strict_fuzzer_seed_corpus.zip $SRC/spif/spif/fixtures/cross_lang/*.spif
+zip -j "$OUT/fuzz_decode_fuzzer_seed_corpus.zip" "$SRC/spif/spif/fixtures/cross_lang/"*.spif
+zip -j "$OUT/fuzz_decode_strict_fuzzer_seed_corpus.zip" "$SRC/spif/spif/fixtures/cross_lang/"*.spif
